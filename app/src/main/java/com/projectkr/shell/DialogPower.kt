c16fd6ff837801007b5f24dd192c076969aa4347
Twmcy <156540799+Twmcy@users.noreply.github.com>
2024-01-28 01:34:42 +0800

    加入电源提示

--- app/src/main/java/com/projectkr/shell/DialogPower.kt
+++ app/src/main/java/com/projectkr/shell/DialogPower.kt
@@ -1,6 +1,7 @@
 package com.projectkr.shell
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.view.View
 import com.omarea.common.shell.KeepShellPublic
 import com.omarea.common.ui.DialogHelper
@@ -8,31 +9,45 @@ import com.omarea.common.ui.DialogHelper
 class DialogPower(var context: Activity) {
     fun showPowerMenu() {
         val view = context.layoutInflater.inflate(R.layout.dialog_power_operation, null)
-        val dialog = DialogHelper.customDialog(context, view)
+        
+        val builder = AlertDialog.Builder(context)
+        builder.setView(view)
+        val dialog = builder.create()
+
         view.findViewById<View>(R.id.power_shutdown).setOnClickListener {
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_shutdown_cmd))
+            showConfirmationDialog(context.getString(R.string.power_shutdown_cmd))
         }
         view.findViewById<View>(R.id.power_reboot).setOnClickListener {
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_reboot_cmd))
+            showConfirmationDialog(context.getString(R.string.power_reboot_cmd))
         }
         view.findViewById<View>(R.id.power_hot_reboot).setOnClickListener {
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_hot_reboot_cmd))
+            showConfirmationDialog(context.getString(R.string.power_hot_reboot_cmd))
         }
-
         view.findViewById<View>(R.id.power_recovery).setOnClickListener {
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_recovery_cmd))
+            showConfirmationDialog(context.getString(R.string.power_recovery_cmd))
         }
         view.findViewById<View>(R.id.power_fastboot).setOnClickListener {
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_fastboot_cmd))
+            showConfirmationDialog(context.getString(R.string.power_fastboot_cmd))
         }
         view.findViewById<View>(R.id.power_emergency).setOnClickListener {
             dialog.dismiss()
-            KeepShellPublic.doCmdSync(context.getString(R.string.power_emergency_cmd))
+            showConfirmationDialog(context.getString(R.string.power_emergency_cmd))
         }
+    }
+
+    private fun showConfirmationDialog(cmd: String) {
+        AlertDialog.Builder(context)
+            .setTitle("确认操作")
+            .setMessage("确定要执行该操作吗？")
+            .setPositiveButton("是") { _, _ ->
+                KeepShellPublic.doCmdSync(cmd)
+            }
+            .setNegativeButton("否") { _, _ -> }
+            .show()
     }
 }
